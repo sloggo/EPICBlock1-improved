09377d737c56c409058ec7b6e07bbf92d7da710a
sloggo <70816908+sloggo@users.noreply.github.com>
2023-11-13 17:20:27 +0000

    Hash passwords using Bcrypt and store it in password instead of string

--- questiongame/src/main/java/questiongame/User.java
+++ questiongame/src/main/java/questiongame/User.java
@@ -1,6 +1,7 @@
 package questiongame;
 
 import org.bson.types.ObjectId;
+import org.mindrot.jbcrypt.BCrypt;
 
 public class User {
     String userId;
@@ -11,7 +12,11 @@ public class User {
     public User(String userId, String username, String password, int score){
         this.userId = userId;
         this.username = username;
-        this.password = password;
+        this.password = hashPass(password, "securetoken");
         this.score = score;
+    }
+
+    public String hashPass(String passwordInput, String pepper){
+        return BCrypt.hashpw(passwordInput, BCrypt.gensalt()+pepper);
     }
 }
